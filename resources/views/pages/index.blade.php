57f348cc20f2d940021f6d52534737f476b82a1e
Jasmin Mediani <molihua1992@gmail.com>
2020-12-01 22:33:39 +0100

    home e about responsive

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -4,8 +4,7 @@
     <div class="sezione sezione-home">
         {{-- <h1>{{$titolo}}</h1>  il titolo che si trova nella funzione pubblica index, in PagesController --}}
         {{-- <p class="text-center">Questa è la pagina index in resources/views/pages</p> --}}
-        <div class="semi-hero">
-        </div>
+        <div class="semi-hero"></div>
         <div class="testo-jumbo">
             <h2>We’re a <i>creative</i> studio for women who mean <i>business</i></h2>
             <h3>We visualize your brand's highest self – by design.</h3>
